c17b4d93db24d84da9ea2ab6b9a166c36922460d
truds99 <dudscardoso9@gmail.com>
2024-06-27 02:37:18 -0300

    add post sign-up

--- src/app.js
+++ src/app.js
@@ -2,12 +2,16 @@ import express, { json } from "express";
 import cors from 'cors';
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
+import httpStatus from "http-status";
+import joi from "joi";
 
 const app = express();
 app.use(cors());
 app.use(json());
 dotenv.config();
 
+
+// DB Connection
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
 mongoClient.connect()
@@ -17,5 +21,37 @@ mongoClient.connect()
     })
     .catch((err) => console.log(err.message));
 
+
+// Schemas
+const userSchema = joi.object({
+     username: joi.string().required().trim(),
+     avatar: joi.string().required().trim()
+});
+
+
+// Endpoints
+app.post('/sign-up', async (req, res) => {
+    const { username, avatar } = req.body;
+    const validation = userSchema.validate(req.body, { abortEarly: false });
+
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(httpStatus.UNPROCESSABLE_ENTITY).send(errors);
+    }
+
+    try { 
+        await db.collection("users").insertOne({
+            username,
+            avatar
+        });
+        res.sendStatus(httpStatus.CREATED);
+    }
+    catch (err){
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err.message);
+    } 
+});
+
+
+// PORT
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is up and running on port ${port}`));
